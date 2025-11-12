c33a0ec58f1a09dc0db39d7ab01023f344ba1bb4
Aqil Səidov <aqilasa638@gmail.com>
2025-11-13 03:06:14 +0400

    Translation History model created(structure might change)

---
+++ src/main/java/com/project/translate/model/History.java
@@ -0,0 +1,39 @@
+package com.project.translate.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "translation_history")
+public class History {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "source_text", nullable = false)
+    private String sourceText;
+
+    @Column(name = "translated_text", nullable = false)
+    private String translatedText;
+
+    @Column(name = "source_lang", nullable = false)
+    private String sourceLang;
+
+    @Column(name = "target_lang", nullable = false)
+    private String targetLang;
+
+    @Column(name = "translated_at")
+    @CreationTimestamp
+    private LocalDateTime translatedAt;
+}
---
+++ src/main/resources/sql/history.sql
@@ -0,0 +1,11 @@
+CREATE TABLE translation_history(
+            id BIGINT PRIMARY KEY AUTO_INCREMENT,
+            user_id BIGINT NOT NULL,
+            source_text TEXT NOT NULL,
+            translated_text TEXT NOT NULL,
+            source_lang VARCHAR(10) NOT NULL,
+            target_lang VARCHAR(10) NOT NULL,
+            translated_at TIMESTAMP
+);
+CREATE INDEX idx_user_id ON translation_history(user_id);
+CREATE INDEX idx_user_date ON translation_history(user_id, translated_at DESC);
